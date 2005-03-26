f1f40c7ae642465fc3aecb98d85fd31dd850fec6
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2005-03-26 18:13:22 +0000

    removed outdated VC project files

--- qrazor-fx3/data/xreal/glsl/lighting_D_proj_fp.glsl
+++ qrazor-fx3/data/xreal/glsl/lighting_D_proj_fp.glsl
@@ -22,7 +22,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 uniform sampler2D	u_diffusemap;
 uniform sampler2D	u_attenuationmap_xy;
-uniform sampler2D	u_shadowmap;
+uniform sampler2DShadow	u_shadowmap;
 uniform vec3		u_light_origin;
 uniform vec3		u_light_color;
 
@@ -48,7 +48,7 @@ void	main()
 	vec3 attenuation_xy = var_tex_atten_xy_z.w < 0.0 ? vec3(0.0, 0.0, 0.0) : texture2DProj(u_attenuationmap_xy, var_tex_atten_xy_z.xyw).rgb;
 	
 	// compute shadow
-	float shadow = shadow2DProj(u_shadowmap, var_tex_shadow);
+	vec3 shadow = shadow2DProj(u_shadowmap, var_tex_shadow).rgb;
 //	vec3 shadow = var_tex_shadow.w < 0.0 ? vec3(0.0, 0.0, 0.0) : texture2DProj(u_shadowmap, var_tex_shadow).rgb;
 //	vec3 shadow = var_tex_shadow.w < 0.0 ? vec3(0.0, 0.0, 0.0) : texture2DProj(u_attenuationmap_xy, var_tex_shadow.xyw).rgb;
 	
