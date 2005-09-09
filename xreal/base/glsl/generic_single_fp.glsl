f8c0c7f1e477d3e91195055e7e28ee98f674f95e
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2005-09-09 13:15:45 +0000

    added generic single texturing shader

---
+++ xreal/base/glsl/generic_single_fp.glsl
@@ -0,0 +1,32 @@
+/// ============================================================================
+/*
+Copyright (C) 2005 Robert Beckebans <trebor_7@users.sourceforge.net>
+Please see the file "AUTHORS" for a list of contributors
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+/// ============================================================================
+
+uniform sampler2D	u_ColorMap;
+
+varying vec2		var_Tex;
+varying vec4		var_Color;
+
+void	main()
+{
+	gl_FragColor = texture2D(u_ColorMap, var_Tex);
+	gl_FragColor *= var_Color;
+}
