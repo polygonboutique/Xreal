7ee75ba8533eb67edfb050b69cc7113d7087aed9
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2005-10-07 11:24:20 +0000

    added texture loading

--- xreal/blender/md3_import.py
+++ xreal/blender/md3_import.py
@@ -1,7 +1,7 @@
 #!BPY
 
-""" Registration info for Blender menus:
-Name: 'Quake3 (.md3)'
+"""
+Name: 'Quake3 (.md3)...'
 Blender: 237
 Group: 'Import'
 Tooltip: 'Import from Quake3 file format. (.md3)'
@@ -9,7 +9,7 @@ Tooltip: 'Import from Quake3 file format. (.md3)'
 
 __author__ = "Bob Holcomb, Robert (Tr3B) Beckebans"
 __url__ = ("http://xreal.sourceforge.net")
-__version__ = "0.3 2004-10-21"
+__version__ = "0.4 2005-10-7"
 
 __bpydoc__ = """\
 This script imports a Quake 3 file (MD3), textures, 
@@ -58,6 +58,7 @@ MD3_MAX_SHADERS = 256
 MD3_MAX_VERTICES = 4096
 MD3_MAX_TRIANGLES = 8192
 MD3_XYZ_SCALE = (1.0 / 64.0)
+MD3_BLENDER_SCALE = (1.0 / 1.0)
 
 
 class vert:
@@ -601,9 +602,9 @@ def animateMesh(surface, meshObject):
 		# update the vertices
 		for j in range(0,surface.numVerts):
 			# i*sufrace.numVerts+j=where in the surface vertex list the vert position for this frame is
-			x = surface.verts[(i * surface.numVerts) + j].xyz[0] * (MD3_XYZ_SCALE) #*g_scale.val
-			y = surface.verts[(i * surface.numVerts) + j].xyz[1] * (MD3_XYZ_SCALE) #*g_scale.val
-			z = surface.verts[(i * surface.numVerts) + j].xyz[2] * (MD3_XYZ_SCALE) #*g_scale.val
+			x = surface.verts[(i * surface.numVerts) + j].xyz[0] * (MD3_XYZ_SCALE) * (MD3_BLENDER_SCALE)
+			y = surface.verts[(i * surface.numVerts) + j].xyz[1] * (MD3_XYZ_SCALE) * (MD3_BLENDER_SCALE)
+			z = surface.verts[(i * surface.numVerts) + j].xyz[2] * (MD3_XYZ_SCALE) * (MD3_BLENDER_SCALE)
 
 			# put the vertex in the right spot
 			mesh.verts[j].co[0] = y
@@ -638,29 +639,55 @@ def animateMesh(surface, meshObject):
 		# not really necissary, but I like playing with the frame counter
 		Blender.Set("curframe", i)
 
+# strips the slashes from the back of a string
+def stripPath(path):
+	for CH in range(len(path), 0, -1):
+		if path[CH-1] == "/" or path[CH-1] == "\\":
+			path = path[CH:]
+			break
+	return path
+
+# strips file type extension
+def stripExtension(name):
+	return name[:name.find('.')]
+
 
 def skinMesh(surface, meshObject):
 	mesh = meshObject.getData()
 	
 	for i in range(0, surface.numShaders):
 		# create new material
-		mat = Material.New(surface.shaders[i].name)
+		matName = stripExtension(stripPath(surface.shaders[i].name))
+		mat = Material.New(matName)
 		
 		# create new texture
-		"""
-		texture = Texture.New(surface.shaders[i].name)
+		texture = Texture.New(matName)
 		texture.setType('Image')
-		try:
-			image = Image.Load('/opt/xreal/base' + surface.shaders[i].name)
-		except:
-			print "unable to open /opt/xreal/base", surface.shaders[i].name
-			continue
-			
-		texture.image = image
 		
-		# assign texture to material
+		# NOTE: change this to your installation directory
+		try:
+			# try .tga by default
+			imageName = stripExtension('/opt/xreal/base/' + surface.shaders[i].name) + '.tga'
+			image = Image.Load(imageName)
+			
+			# assign image to texture
+			texture.image = image
+		except:
+			try:
+				imageName = stripExtension(imageName) + '.jpg'
+				image = Image.Load(imageName)
+				
+				# assign image to texture
+				texture.image = image
+			except:
+				print "unable to load image ", imageName
+			
+		#print "Image from", image.getFilename()
+		#print "loaded to obj", image.getName()
+		
+		# texture to material
 		mat.setTexture(0, texture, Texture.TexCo.UV, Texture.MapTo.COL)
-		"""
+
 		# and append it to the mesh's list of mats
    		mesh.materials.append(mat)
 		
@@ -682,13 +709,13 @@ def loadModel(filename):
 
 		# make the verts
 		for i in range (0, surface.numVerts):
-			x = surface.verts[i].xyz[0] * (MD3_XYZ_SCALE) #*g_scale.val
-			y = surface.verts[i].xyz[1] * (MD3_XYZ_SCALE) #*g_scale.val
-			z = surface.verts[i].xyz[2] * (MD3_XYZ_SCALE) #*g_scale.val
+			x = surface.verts[i].xyz[0] * (MD3_XYZ_SCALE) * (MD3_BLENDER_SCALE)
+			y = surface.verts[i].xyz[1] * (MD3_XYZ_SCALE) * (MD3_BLENDER_SCALE)
+			z = surface.verts[i].xyz[2] * (MD3_XYZ_SCALE) * (MD3_BLENDER_SCALE)
 			vertex = NMesh.Vert(y, -x, z)
 			mesh.verts.append(vertex)
 	
-		# mke the UV list
+		# make the UV list
 		mesh.hasFaceUV(1)  #turn on face UV coordinates for this mesh
 		for tex_coord in surface.uv:
 			u = tex_coord.u
