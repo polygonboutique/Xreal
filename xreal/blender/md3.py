389bd3c2c1b30937f4d1022988aff09a37030f76
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2005-10-08 12:45:14 +0000

    refactored md3 code, added exporter

---
+++ xreal/blender/md3.py
@@ -0,0 +1,625 @@
+import sys, struct, string, math
+from types import *
+
+import os
+from os import path
+
+def asciiz (s):
+  n = 0
+  while (ord(s[n]) != 0):
+    n = n + 1
+  return s[0:n]
+
+MD3_IDENT = "IDP3"
+MD3_VERSION = 15
+MD3_MAX_QPATH = 64
+MD3_MAX_TAGS = 16
+MD3_MAX_SURFACES = 32
+MD3_MAX_FRAMES = 1024
+MD3_MAX_SHADERS = 256
+MD3_MAX_VERTICES = 4096
+MD3_MAX_TRIANGLES = 8192
+MD3_XYZ_SCALE = (1.0 / 64.0)
+MD3_BLENDER_SCALE = (1.0 / 1.0)
+
+class md3Vert:
+	xyz = []
+	normal = 0
+	binaryFormat = "<3hh"
+	
+	def __init__(self):
+		self.xyz = [0, 0, 0]
+		self.normal = 0
+		
+	def getSize(self):
+		return struct.calcsize(self.binaryFormat)
+	
+	# copied from PhaethonH <phaethon@linux.ucla.edu> md3.py
+	def decode(self, latlng):
+		lat = (latlng >> 8) & 0xFF;
+		lng = (latlng) & 0xFF;
+		lat *= math.pi / 128;
+		lng *= math.pi / 128;
+		x = math.cos(lat) * math.sin(lng)
+		y = math.sin(lat) * math.sin(lng)
+		z =                 math.cos(lng)
+		retval = [ x, y, z ]
+		return retval
+	
+	# copied from PhaethonH <phaethon@linux.ucla.edu> md3.py
+	def encode(self, normal):
+		x, y, z = normal
+		
+		# normalise
+		l = math.sqrt((x*x) + (y*y) + (z*z))
+		if l == 0:
+			return 0
+		x = x/l
+		y = y/l
+		z = z/l
+		
+		if (x == 0.0) & (y == 0.0) :
+			if z > 0.0:
+				return 0
+			else:
+				return (128 << 8)
+		
+		# encode a normal vector into a 16-bit latitude-longitude value
+		#lng = math.acos(z)
+		#lat = math.acos(x / math.sin(lng))
+		#retval = ((lat & 0xFF) << 8) | (lng & 0xFF)
+		lng = math.acos(z) * 255 / (2 * math.pi)
+		lat = math.atan2(y, x) * 255 / (2 * math.pi)
+		retval = ((int(lat) & 0xFF) << 8) | (int(lng) & 0xFF)
+		return retval
+	
+	def load(self, file):
+		tmpData = file.read(struct.calcsize(self.binaryFormat))
+		data = struct.unpack(self.binaryFormat, tmpData)
+		self.xyz[0] = data[0]
+		self.xyz[1] = data[1]
+		self.xyz[2] = data[2]
+		self.normal = data[3]
+		return self
+		
+	def save(self, file):
+		tmpData = [0] * 4
+		tmpData[0] = self.xyz[0] / MD3_XYZ_SCALE
+		tmpData[1] = self.xyz[1] / MD3_XYZ_SCALE
+		tmpData[2] = self.xyz[2] / MD3_XYZ_SCALE
+		tmpData[3] = self.normal
+		data = struct.pack(self.binaryFormat, tmpData[0], tmpData[1], tmpData[2], tmpData[3])
+		file.write(data)
+		#print "Wrote MD3 Vertex: ", data
+	
+	def dump(self):
+		print "MD3 Vertex Structure"
+		print "X: ", self.xyz[0]
+		print "Y: ", self.xyz[1]
+		print "Z: ", self.xyz[2]
+		print "Normal: ", self.normal
+		print ""
+		
+class md3TexCoord:
+	u = 0.0
+	v = 0.0
+
+	binaryFormat = "<2f"
+
+	def __init__(self):
+		self.u = 0.0
+		self.v = 0.0
+		
+	def getSize(self):
+		return struct.calcsize(self.binaryFormat)
+
+	def load(self, file):
+		tmpData = file.read(struct.calcsize(self.binaryFormat))
+		data = struct.unpack(self.binaryFormat, tmpData)
+		self.u = data[0]
+		self.v = data[1]
+		return self
+
+	def save(self, file):
+		tmpData = [0] * 2
+		tmpData[0] = self.u
+		tmpData[1] = 1.0 - self.v
+		data = struct.pack(self.binaryFormat, tmpData[0], tmpData[1])
+		file.write(data)
+		#print "wrote MD3 texture coordinate structure: ",data
+
+	def dump(self):
+		print "MD3 Texture Coordinate Structure"
+		print "texture coordinate u: ", self.u
+		print "texture coordinate v: ", self.v
+		print ""
+		
+
+class md3Triangle:
+	indexes = []
+
+	binaryFormat = "<3i"
+
+	def __init__(self):
+		self.indexes = [ 0, 0, 0 ]
+		
+	def getSize(self):
+		return struct.calcsize(self.binaryFormat)
+
+	def load(self, file):
+		tmpData = file.read(struct.calcsize(self.binaryFormat))
+		data = struct.unpack(self.binaryFormat, tmpData)
+		self.indexes[0] = data[0]
+		self.indexes[1] = data[1]
+		self.indexes[2] = data[2]
+		return self
+
+	def save(self, file):
+		tmpData = [0] * 3
+		tmpData[0] = self.indexes[0]
+		tmpData[1] = self.indexes[2] # reverse
+		tmpData[2] = self.indexes[1] # reverse
+		data = struct.pack(self.binaryFormat,tmpData[0], tmpData[1], tmpData[2])
+		file.write(data)
+		#print "wrote MD3 face structure: ",data
+
+	def dump(self):
+		print "MD3 Triangle Structure"
+		print "vertex index: ", self.indexes[0]
+		print "vertex index: ", self.indexes[1]
+		print "vertex index: ", self.indexes[2]
+		print ""
+
+
+class md3Shader:
+	name = ""
+	index = 0
+	
+	binaryFormat = "<%dsi" % MD3_MAX_QPATH
+
+	def __init__(self):
+		self.name = ""
+		self.index = 0
+		
+	def getSize(self):
+		return struct.calcsize(self.binaryFormat)
+
+	def load(self, file):
+		tmpData = file.read(struct.calcsize(self.binaryFormat))
+		data = struct.unpack(self.binaryFormat, tmpData)
+		self.name = asciiz(data[0])
+		self.index = data[1]
+		return self
+
+	def save(self, file):
+		tmpData[0] = self.name
+		tmpData[1] = self.index
+		data = struct.pack(self.binaryFormat, tmpData[0], tmpData[1])
+		file.write(data)
+		#print "wrote MD3 shader structure: ",data
+
+	def dump (self):
+		print "MD3 Shader"
+		print "shader name: ", self.name
+		print "shader index: ", self.index
+		print ""
+
+
+class md3Surface:
+	ident = ""
+	name = ""
+	flags = 0
+	numFrames = 0
+	numShaders = 0
+	numVerts = 0
+	numTriangles = 0
+	ofsTriangles = 0
+	ofsShaders = 0
+	ofsUV = 0
+	ofsVerts = 0
+	ofsEnd = 0
+	shaders = []
+	triangles = []
+	uv = []
+	verts = []
+	
+	binaryFormat = "<4s%ds10i" % MD3_MAX_QPATH  # 1 int, name, then 10 ints
+	
+	def __init__(self):
+		self.ident = ""
+		self.name = ""
+		self.flags = 0
+		self.numFrames = 0
+		self.numShaders = 0
+		self.numVerts = 0
+		self.numTriangles = 0
+		self.ofsTriangles = 0
+		self.ofsShaders = 0
+		self.ofsUV = 0
+		self.ofsVerts = 0
+		self.ofsEnd
+		self.shaders = []
+		self.triangles = []
+		self.uv = []
+		self.verts = []
+		
+	def getSize(self):
+		sz = struct.calcsize(self.binaryFormat)
+		self.ofsShaders = sz
+		for s in self.shaders:
+			sz += s.getSize()
+		self.ofsTriangles = sz
+		for t in self.triangles:
+			sz += t.getSize()
+		self.ofsUV = sz
+		for u in self.uv:
+			sz += u.getSize()
+		self.ofsVerts = sz
+		for v in self.verts:
+			sz += v.getSize()
+		self.ofsEnd = sz
+		return self.ofsEnd
+		
+	def load(self, file):
+		# where are we in the file (for calculating real offsets)
+		ofsBegin = file.tell()
+		tmpData = file.read(struct.calcsize(self.binaryFormat))
+		data = struct.unpack(self.binaryFormat, tmpData)
+		self.ident = data[0]
+		self.name = asciiz(data[1])
+		self.flags = data[2]
+		self.numFrames = data[3]
+		self.numShaders = data[4]
+		self.numVerts = data[5]
+		self.numTriangles = data[6]
+		self.ofsTriangles = data[7]
+		self.ofsShaders = data[8]
+		self.ofsUV = data[9]
+		self.ofsVerts = data[10]
+		self.ofsEnd = data[11]
+		
+		# load the tri info
+		file.seek(ofsBegin + self.ofsTriangles, 0)
+		for i in range(0, self.numTriangles):
+			self.triangles.append(md3Triangle())
+			self.triangles[i].load(file)
+			#self.triangles[i].dump()
+		
+		# load the shader info
+		file.seek(ofsBegin + self.ofsShaders, 0)
+		for i in range(0, self.numShaders):
+			self.shaders.append(md3Shader())
+			self.shaders[i].load(file)
+			self.shaders[i].dump()
+			
+		# load the uv info
+		file.seek(ofsBegin + self.ofsUV, 0)
+		for i in range(0, self.numVerts):
+			self.uv.append(md3TexCoord())
+			self.uv[i].load(file)
+			#self.uv[i].dump()
+			
+		# load the verts info
+		file.seek(ofsBegin + self.ofsVerts, 0)
+		for i in range(0, self.numFrames):
+			for j in range(0, self.numVerts):
+				self.verts.append(md3Vert())
+				#i*self.numVerts+j=where in the surface vertex list the vert position for this frame is
+				self.verts[(i * self.numVerts) + j].load(file)
+				#self.verts[j].dump()
+			
+		# go to the end of this structure
+		file.seek(ofsBegin+self.ofsEnd, 0)
+			
+		return self
+	
+	def save(self, file):
+		self.getSize()
+		tmpData = [0] * 12
+		tmpData[0] = self.ident
+		tmpData[1] = self.name
+		tmpData[2] = self.flags
+		tmpData[3] = self.numFrames
+		tmpData[4] = self.numShaders
+		tmpData[5] = self.numVerts
+		tmpData[6] = self.numTriangles
+		tmpData[7] = self.ofsTriangles
+		tmpData[8] = self.ofsShaders
+		tmpData[9] = self.ofsUV
+		tmpData[10] = self.ofsVerts
+		tmpData[11] = self.ofsEnd
+		data = struct.pack(self.binaryFormat, tmpData[0],tmpData[1],tmpData[2],tmpData[3],tmpData[4],tmpData[5],tmpData[6],tmpData[7],tmpData[8],tmpData[9],tmpData[10],tmpData[11])
+		file.write(data)
+
+		# write the tri data
+		for t in self.triangles:
+			t.save(file)
+
+		# save the shader coordinates
+		for s in self.shaders:
+			s.save(file)
+
+		#save the uv info
+		for u in self.uv:
+			u.save(file)
+
+		#save the verts
+		for v in self.verts:
+			v.save(file)
+
+	def dump(self):
+		print "MD3 Surface"
+		print "Ident: ", self.ident
+		print "Name: ", self.name
+		print "Flags: ", self.flags
+		print "Number of Frames: ", self.numFrames
+		print "Number of Shaders: ", self.numShaders
+		print "Number of Verts: ", self.numVerts
+		print "Number of Triangles: ", self.numTriangles
+		print "Offset to Triangles: ", self.ofsTriangles
+		print "Offset to Shaders: ", self.ofsShaders
+		print "Offset to UV: ", self.ofsUV
+		print "Offset to Verts: ", self.ofsVerts
+		print "Offset to end: ", self.ofsEnd
+		print ""
+		
+
+class md3Tag:
+	name = ""
+	origin = []
+	axis = []
+	
+	binaryFormat="<%ds3f9f" % MD3_MAX_QPATH
+	
+	def __init__(self):
+		self.name = ""
+		self.origin = [0, 0, 0]
+		self.axis = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+		
+	def getSize(self):
+		return struct.calcsize(self.binaryFormat)
+		
+	def load(self, file):
+		tmpData = file.read(struct.calcsize(self.binaryFormat))
+		data = struct.unpack(self.binaryFormat, tmpData)
+		self.name = asciiz(data[0])
+		self.origin[0] = data[1]
+		self.origin[1] = data[2]
+		self.origin[2] = data[3]
+		self.axis[0] = data[4]
+		self.axis[1] = data[5]
+		self.axis[2] = data[6]
+		self.axis[3] = data[7]
+		self.axis[4] = data[8]
+		self.axis[5] = data[9]
+		self.axis[6] = data[10]
+		self.axis[7] = data[11]
+		self.axis[8] = data[12]
+		return self
+		
+	def save(self, file):
+		tmpData = [0] * 13
+		tmpData[0] = self.name
+		tmpData[1] = float(self.origin[0])
+		tmpData[2] = float(self.origin[1])
+		tmpData[3] = float(self.origin[2])
+		tmpData[4] = float(self.axis[0])
+		tmpData[5] = float(self.axis[1])
+		tmpData[6] = float(self.axis[2])
+		tmpData[7] = float(self.axis[3])
+		tmpData[8] = float(self.axis[4])
+		tmpData[9] = float(self.axis[5])
+		tmpData[10] = float(self.axis[6])
+		tmpData[11] = float(self.axis[7])
+		tmpData[12] = float(self.axis[8])
+		data = struct.pack(self.binaryFormat, tmpData[0],tmpData[1],tmpData[2],tmpData[3],tmpData[4],tmpData[5],tmpData[6], tmpData[7], tmpData[8], tmpData[9], tmpData[10], tmpData[11], tmpData[12])
+		file.write(data)
+		#print "wrote MD3 Tag structure: ",data
+		
+	def dump(self):
+		print "MD3 Tag"
+		print "Name: ", self.name
+		print "Origin: ", self.origin
+		print "Axis: ", self.axis
+		print ""
+	
+class md3Frame:
+	mins = 0
+	maxs = 0
+	localOrigin = 0
+	radius = 0.0
+	name = ""
+	
+	binaryFormat="<3f3f3ff16s"
+	
+	def __init__(self):
+		self.mins = [0, 0, 0]
+		self.maxs = [0, 0, 0]
+		self.localOrigin = [0, 0, 0]
+		self.radius = 0.0
+		self.name = ""
+		
+	def getSize(self):
+		return struct.calcsize(self.binaryFormat)
+
+	def load(self, file):
+		tmpData = file.read(struct.calcsize(self.binaryFormat))
+		data = struct.unpack(self.binaryFormat, tmpData)
+		self.mins[0] = data[0]
+		self.mins[1] = data[1]
+		self.mins[2] = data[2]
+		self.maxs[0] = data[3]
+		self.maxs[1] = data[4]
+		self.maxs[2] = data[5]
+		self.localOrigin[0] = data[6]
+		self.localOrigin[1] = data[7]
+		self.localOrigin[2] = data[8]
+		self.radius = data[9]
+		self.name = asciiz(data[10])
+		return self
+
+	def save(self, file):
+		tmpData = [0] * 11
+		tmpData[0] = self.mins[0]
+		tmpData[1] = self.mins[1]
+		tmpData[2] = self.mins[2]
+		tmpData[3] = self.maxs[0]
+		tmpData[4] = self.maxs[1]
+		tmpData[5] = self.maxs[2]
+		tmpData[6] = self.localOrigin[0]
+		tmpData[7] = self.localOrigin[1]
+		tmpData[8] = self.localOrigin[2]
+		tmpData[9] = self.radius
+		tmpData[10] = self.name
+		data = struct.pack(self.binaryFormat, tmpData[0],tmpData[1],tmpData[2],tmpData[3],tmpData[4],tmpData[5],tmpData[6],tmpData[7], tmpData[8], tmpData[9], tmpData[10])
+		file.write(data)
+		#print "wrote MD3 frame structure: ",data
+
+	def dump(self):
+		print "MD3 Frame"
+		print "Min Bounds: ", self.mins
+		print "Max Bounds: ", self.maxs
+		print "Local Origin: ", self.localOrigin
+		print "Radius: ", self.radius
+		print "Name: ", self.name
+		print ""
+
+class md3Object:
+	# header structure
+	ident = ""			# this is used to identify the file (must be IDP3)
+	version = 0			# the version number of the file (Must be 15)
+	name = ""
+	flags = 0
+	numFrames = 0
+	numTags = 0
+	numSurfaces = 0
+	numSkins = 0		#don't think this is used, but here anyways
+	ofsFrames = 0
+	ofsTags = 0
+	ofsSurfaces = 0
+	ofsEnd = 0
+	frames = []
+	tags = []
+	surfaces = []
+
+	binaryFormat="<4si%ds9i" % MD3_MAX_QPATH  #little-endian (<), 17 integers (17i)
+
+	def __init__(self):
+		self.ident=0
+		self.version=0
+		self.name=""
+		self.flags=0
+		self.numFrames=0
+		self.numTags=0
+		self.numSurfaces=0
+		self.numSkins=0
+		self.ofsFrames=0
+		self.ofsTags=0
+		self.ofsSurfaces=0
+		self.ofsEnd=0
+		self.frames=[]
+		self.tags=[]
+		self.surfaces=[]
+
+	def getSize(self):
+		self.ofsFrames = struct.calcsize(self.binaryFormat)
+		self.ofsTags = self.ofsFrames
+		for f in self.frames:
+			self.ofsTags += f.getSize()
+		self.ofsSurfaces += self.ofsTags
+		for t in self.tags:
+			self.ofsSurfaces += t.getSize()
+		self.ofsEnd = self.ofsSurfaces
+		for s in self.surfaces:
+			self.ofsEnd += s.getSize()
+		return self.ofsEnd
+
+	def load(self, file):
+		tmpData = file.read(struct.calcsize(self.binaryFormat))
+		data = struct.unpack(self.binaryFormat, tmpData)
+
+		self.ident = data[0]
+		self.version = data[1]
+
+		if(self.ident != "IDP3" or self.version != 15):
+			print "Not a valid MD3 file"
+			print "Ident=", self.ident
+			print "Version=",self.version
+			Exit()
+
+		self.name = asciiz(data[2])
+		self.flags = data[3]
+		self.numFrames = data[4]
+		self.numTags = data[5]
+		self.numSurfaces = data[6]
+		self.numSkins = data[7]
+		self.ofsFrames = data[8]
+		self.ofsTags = data[9]
+		self.ofsSurfaces = data[10]
+		self.ofsEnd = data[11]
+
+		# load the frame info
+		file.seek(self.ofsFrames, 0)
+		for i in range(0, self.numFrames):
+			self.frames.append(md3Frame())
+			self.frames[i].load(file)
+			#self.frames[i].dump()
+		
+		# load the tags info
+		file.seek(self.ofsTags, 0)
+		for i in range(0, self.numTags):
+			self.tags.append(md3Tag())
+			self.tags[i].load(file)
+			self.tags[i].dump()
+		
+		# load the surface info
+		file.seek(self.ofsSurfaces, 0)
+		for i in range(0, self.numSurfaces):
+			self.surfaces.append(md3Surface())
+			self.surfaces[i].load(file)
+			self.surfaces[i].dump()
+		return self
+
+	def save(self, file):
+		self.getSize()
+		tmpData = [0] * 12
+		tmpData[0] = self.ident
+		tmpData[1] = self.version
+		tmpData[2] = self.name
+		tmpData[3] = self.flags
+		tmpData[4] = self.numFrames
+		tmpData[5] = self.numTags
+		tmpData[6] = self.numSurfaces
+		tmpData[7] = self.numSkins
+		tmpData[8] = self.ofsFrames
+		tmpData[9] = self.ofsTags
+		tmpData[10] = self.ofsSurfaces
+		tmpData[11] = self.ofsEnd
+
+		data=struct.pack(self.binaryFormat, tmpData[0],tmpData[1],tmpData[2],tmpData[3],tmpData[4],tmpData[5],tmpData[6],tmpData[7], tmpData[8], tmpData[9], tmpData[10], tmpData[11])
+		file.write(data)
+
+		for f in self.frames:
+			f.save(file)
+			
+		for t in self.tags:
+			t.save(file)
+			
+		for s in self.surfaces:
+			s.save(file)
+
+	def dump(self):
+		print "Header Information"
+		print "ident: ", self.ident
+		print "version: ", self.version
+		print "Name: ", self.name
+		print "Flags: ", self.flags
+		print "Number of Frames: ",self.numFrames
+		print "Number of Tags: ", self.numTags
+		print "Number of Surfaces: ", self.numSurfaces
+		print "Number of Skins: ", self.numSkins
+		print "Offset Frames: ", self.ofsFrames
+		print "Offset Tags: ", self.ofsTags
+		print "Offset Surfaces: ", self.ofsSurfaces
+		print "Offset end: ", self.ofsEnd
+		print ""
---
+++ xreal/blender/md3_export.py
@@ -0,0 +1,265 @@
+#!BPY
+
+"""
+Name: 'Quake3 (.md3)...'
+Blender: 237
+Group: 'Export'
+Tooltip: 'Export to Quake3 file format. (.md3)'
+"""
+__author__ = "PhaethonH, Bob Holcomb, Damien McGinnes, Robert (Tr3B) Beckebans"
+__url__ = ("http://xreal.sourceforge.net")
+__version__ = "0.2 2005-10-7"
+
+__bpydoc__ = """\
+This script exports a Quake3 file (MD3).
+
+Supported:<br>
+    TODO
+
+Missing:<br>
+    TODO
+
+Known issues:<br>
+    None.
+
+Notes:<br>
+    TODO
+"""
+
+import Blender
+from Blender import *
+from Blender import NMesh, Object, Material, Image, Texture
+
+import sys, struct, string, math
+from types import *
+
+import os
+from os import path
+
+import md3
+from md3 import *
+
+
+NUM_FRAMES = 1
+EXPORT_ALL = 0   # export only selected objs, or all?
+
+def applyTransform(vert, matrix):
+	vertCopy = Mathutils.CopyVec(vert)
+	vertCopy.resize4D()
+	return Mathutils.VecMultMat(vertCopy, matrix)
+
+
+def updateFrameData(v, f):
+	for i in range(0, 3):
+		f.mins[i] = min(v[i], f.mins[i])
+	for i in range(0, 3):
+		f.maxs[i] = max(v[i], f.mins[i])
+
+
+def processSurface(mesh_obj, md3):
+	# because md3 doesnt suppoort faceUVs like blender, we need to duplicate
+	# any vertex that has multiple uv coords
+
+	vert_dict = {} 
+	index_dict = {} # maps a vertex index to the revised index after duplicating to account for uv
+	vert_list = [] # list of vertices ordered by revised index
+	vert_count = 0
+	tex_list = [] # list of tex coords ordered by revised index
+	face_list = [] # list of faces (they index into vert_list)
+	face_count = 0
+
+	Blender.Set("curframe", 1)
+
+	#get access to the mesh data (as at frame #1)
+	mesh = NMesh.GetRawFromObject(mesh_obj.name)	
+	matrix = mesh_obj.getMatrix('worldspace')
+
+	surf = md3Surface()
+	surf.numFrames = md3.numFrames
+	surf.name = mesh_obj.getName()
+	surf.ident = MD3_IDENT
+
+	#process each face in the mesh
+	for face in mesh.faces:
+		tris_in_this_face = []  #to handle quads and up...
+		
+		# this makes a list of indices for each tri in this face. a quad will be [[0,1,1],[0,2,3]]
+		for vi in range(1, len(face.v)-1):
+			tris_in_this_face.append([0, vi, vi + 1])
+		
+		#loop across each tri in the face, then each vertex in the tri
+		for this_tri in tris_in_this_face:
+			face_count += 1
+			tri = md3Triangle()
+			tri_ind = 0
+			for i in this_tri:
+				#get the vertex index, coords and uv coords
+				index = face.v[i].index
+				v = face.v[i].co
+				if mesh.hasFaceUV():
+					uv = face.uv[i]
+				elif mesh.hasVertexUV():
+					uv = (face.v[i].uvco[0], face.v[i].uvco[1])
+				else:
+					uv = (0.0, 0.0) # handle case with no tex coords	
+
+				
+				if vert_dict.has_key((index, uv)):
+					# if we've seen this exact vertex before, simply add it
+					# to the tris list of vertex indices
+					tri.indexes[tri_ind] = vert_dict[(index, uv)]
+				else:
+					# havent seen this tri before 
+					# (or its uv coord is different, so we need to duplicate it)
+					
+					vert_dict[(index, uv)] = vert_count
+					
+					# put the uv coord into the list
+					# (uv coord are directly related to each vertex)
+					tex = md3TexCoord()
+					tex.u = uv[0]
+					tex.v = uv[1]
+					tex_list.append(tex)
+
+					tri.indexes[tri_ind] = vert_count
+
+					# now because we have created a new index, 
+					# we need a way to link it to the index that
+					# blender returns for NMVert.index
+					if index_dict.has_key(index):
+						# already there - each of the entries against 
+						# this key represents  the same vertex with a
+						# different uv value
+						ilist = index_dict[index]
+						ilist.append(vert_count)
+						index_dict[index] = ilist
+					else:
+						#this is a new one
+						index_dict[index] = [vert_count]
+
+					vert_count += 1
+				tri_ind +=1
+			face_list.append(tri)
+
+	# we're done with faces and uv coords
+	for t in tex_list:
+		surf.uv.append(t)
+
+	for f in face_list:
+		surf.triangles.append(f)
+
+	surf.numTriangles = len(face_list)
+	surf.numVerts = vert_count
+
+	# now vertices are stored as frames -
+	# all vertices for frame 1, all vertices for frame 2...., all vertices for frame n
+	# so we need to iterate across blender's frames, and copy out each vertex
+	for	frameNum in range(1, md3.numFrames + 1):
+		Blender.Set("curframe", frameNum)
+
+		m = NMesh.GetRawFromObject(mesh_obj.name)
+
+		vlist = [0] * vert_count
+		for vertex in m.verts:
+			try:
+				vindices = index_dict[vertex.index]
+			except:
+				print "warning found a vertex in %s that is not part of a face" % mesh_obj.name
+				continue
+
+			vTx = applyTransform(vertex.co, matrix)
+			nTx = applyTransform(vertex.no, matrix)
+			updateFrameData(vTx, md3.frames[frameNum - 1])
+			vert = md3Vert()
+			vert.xyz = vTx[0:3]
+			vert.normal = vert.encode(nTx[0:3])
+			for ind in vindices:  #apply the position to all the duplicated vertices
+				vlist[ind] = vert
+
+		for vl in vlist:
+			surf.verts.append(vl)
+
+	surf.dump()
+	md3.surfaces.append(surf)
+	md3.numSurfaces += 1
+
+
+def saveModel(filename):
+	if(filename.find('.md3', -4) <= 0):
+		filename += '.md3'
+	print "Exporting MD3 format to ", filename
+	
+	md3 = md3Object()
+	md3.ident = MD3_IDENT
+	md3.version = MD3_VERSION
+
+	tag_list=[]
+
+	Blender.Set("curframe", 1)
+
+	# create a bunch of blank frames, they'll be filled in by 'processSurface'
+	md3.numFrames = NUM_FRAMES
+	for i in range(1, NUM_FRAMES + 1):
+		fr = md3Frame()
+		fr.name = "FRAME" + str(i)
+		md3.frames.append(fr)
+
+	# do we export all objects or just the selected ones?
+	if EXPORT_ALL:
+		objlist = Blender.Object.Get()
+	else:
+		objlist = Blender.Object.GetSelected()
+
+
+	# process each object for the export
+	for obj in objlist:
+		# check if it's a mesh object
+		if obj.getType() == "Mesh":
+			print "processing surface", obj.name
+			if len(md3.surfaces) == MD3_MAX_SURFACES:
+				print "hit md3 limit (%i) for number of surfaces, skipping" % MD3_MAX_SURFACES , obj.getName()
+			else:
+				processSurface(obj, md3)
+		elif obj.getType() == "Empty":   #for tags, we just put em in a list so we can process them all together
+			if obj.name[0:4] == "tag_":
+				print "processing tag", obj.name
+				tag_list.append(obj)
+				md3.numTags += 1
+		else:
+			print "Skipping non mesh object", obj.name
+
+	# work out the transforms for the tags for each frame of the export
+	for fr in range(1, NUM_FRAMES + 1):
+		Blender.Set("curframe", fr)
+		for tag in tag_list:
+			t = md3Tag()
+			matrix = tag.getMatrix('worldspace')
+			for i in range(0, 3):
+				#t.axis[0].co[i] = matrix[0][i]
+				#t.axis[1].co[i] = matrix[1][i]
+				#t.axis[2].co[i] = matrix[2][i]
+				t.origin[0] = matrix[3][0]
+				t.origin[1] = matrix[3][1]
+				t.origin[2] = matrix[3][2]
+				
+				t.axis[0] = matrix[0][0]
+				t.axis[1] = matrix[0][1]
+				t.axis[2] = matrix[0][2]
+				
+				t.axis[3] = matrix[1][0]
+				t.axis[4] = matrix[1][1]
+				t.axis[5] = matrix[1][2]
+				
+				t.axis[6] = matrix[2][0]
+				t.axis[7] = matrix[2][1]
+				t.axis[8] = matrix[2][2]
+			t.name = obj.name[4:]
+			md3.tags.append(t)
+
+	# export!
+	file = open(filename, "wb")
+	md3.save(file)
+	file.close()
+	md3.dump()
+
+Blender.Window.FileSelector(saveModel, "Export Quake3 MD3")
