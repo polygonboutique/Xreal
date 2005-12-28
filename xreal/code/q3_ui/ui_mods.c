8f7e4ec554757e84142cbae57d80332f38166d3a
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2005-12-28 23:17:22 +0000

    changed default game to XreaL

--- xreal/code/q3_ui/ui_mods.c
+++ xreal/code/q3_ui/ui_mods.c
@@ -159,9 +159,9 @@ static void UI_Mods_LoadMods( void ) {
 	s_mods.descriptionPtr = s_mods.description;
 	s_mods.fs_gamePtr = s_mods.fs_game;
 
-	// always start off with baseq3
+	// always start off with base
 	s_mods.list.numitems = 1;
-	s_mods.list.itemnames[0] = s_mods.descriptionList[0] = "Quake III Arena";
+	s_mods.list.itemnames[0] = s_mods.descriptionList[0] = "XreaL";
 	s_mods.fs_gameList[0] = "";
 
 	numdirs = trap_FS_GetFileList( "$modlist", "", dirlist, sizeof(dirlist) );
