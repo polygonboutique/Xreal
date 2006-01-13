2e6e33ac2b5d460ef6611d3ead770c24e499f90f
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2006-01-13 18:48:04 +0000

    updated credits

--- xreal/code/q3_ui/ui_credits.c
+++ xreal/code/q3_ui/ui_credits.c
@@ -32,11 +32,12 @@ CREDITS
 #include "ui_local.h"
 
 
-typedef struct {
-	menuframework_s	menu;
+typedef struct
+{
+	menuframework_s menu;
 } creditsmenu_t;
 
-static creditsmenu_t	s_credits;
+static creditsmenu_t s_credits;
 
 
 /*
@@ -44,12 +45,14 @@ static creditsmenu_t	s_credits;
 UI_CreditMenu_Key
 =================
 */
-static sfxHandle_t UI_CreditMenu_Key( int key ) {
-	if( key & K_CHAR_FLAG ) {
+static sfxHandle_t UI_CreditMenu_Key(int key)
+{
+	if(key & K_CHAR_FLAG)
+	{
 		return 0;
 	}
 
-	trap_Cmd_ExecuteText( EXEC_APPEND, "quit\n" );
+	trap_Cmd_ExecuteText(EXEC_APPEND, "quit\n");
 	return 0;
 }
 
@@ -59,58 +62,77 @@ static sfxHandle_t UI_CreditMenu_Key( int key ) {
 UI_CreditMenu_Draw
 ===============
 */
-static void UI_CreditMenu_Draw( void ) {
-	int		y;
+static void UI_CreditMenu_Draw(void)
+{
+	int             y;
 
 	y = 12;
-	UI_DrawProportionalString( 320, y, "id Software is:", UI_CENTER|UI_SMALLFONT, color_white );
+	UI_DrawProportionalString(320, y, "Credits", UI_CENTER | UI_SMALLFONT, colorRed);
 
 	y += 1.42 * PROP_HEIGHT * PROP_SMALL_SIZE_SCALE;
-	UI_DrawProportionalString( 320, y, "Programming", UI_CENTER|UI_SMALLFONT, color_white );
+	UI_DrawProportionalString(320, y, "Programming", UI_CENTER | UI_SMALLFONT, colorLtGrey);
 	y += PROP_HEIGHT * PROP_SMALL_SIZE_SCALE;
-	UI_DrawProportionalString( 320, y, "John Carmack, Robert A. Duffy, Jim Dose'", UI_CENTER|UI_SMALLFONT, color_white );
+	UI_DrawProportionalString(320, y, "Robert 'Tr3B' Beckebans", UI_CENTER | UI_SMALLFONT, colorWhite);
 
 	y += 1.42 * PROP_HEIGHT * PROP_SMALL_SIZE_SCALE;
-	UI_DrawProportionalString( 320, y, "Art", UI_CENTER|UI_SMALLFONT, color_white );
+	UI_DrawProportionalString(320, y, "Art", UI_CENTER | UI_SMALLFONT, colorLtGrey);
 	y += PROP_HEIGHT * PROP_SMALL_SIZE_SCALE;
-	UI_DrawProportionalString( 320, y, "Adrian Carmack, Kevin Cloud,", UI_CENTER|UI_SMALLFONT, color_white );
+	UI_DrawProportionalString(320, y, "Robert 'Tr3B' Beckebans,", UI_CENTER | UI_SMALLFONT, colorWhite);
 	y += PROP_HEIGHT * PROP_SMALL_SIZE_SCALE;
-	UI_DrawProportionalString( 320, y, "Kenneth Scott, Seneca Menard, Fred Nilsson", UI_CENTER|UI_SMALLFONT, color_white );
+	UI_DrawProportionalString(320, y, "Paul 'JTR' Steffens, Lee David Ash,", UI_CENTER | UI_SMALLFONT, colorWhite);
+	y += PROP_HEIGHT * PROP_SMALL_SIZE_SCALE;
+	UI_DrawProportionalString(320, y, "James 'HarlequiN' Taylor,", UI_CENTER | UI_SMALLFONT, colorWhite);
+	y += PROP_HEIGHT * PROP_SMALL_SIZE_SCALE;
+	UI_DrawProportionalString(320, y, "Michael 'mic' Denno", UI_CENTER | UI_SMALLFONT, colorWhite);
+
+	/*
+	y += 1.42 * PROP_HEIGHT * PROP_SMALL_SIZE_SCALE;
+	UI_DrawProportionalString(320, y, "Game Designer", UI_CENTER | UI_SMALLFONT, colorLtGrey);
+	y += PROP_HEIGHT * PROP_SMALL_SIZE_SCALE;
+	UI_DrawProportionalString(320, y, "Graeme Devine", UI_CENTER | UI_SMALLFONT, colorWhite);
+	*/
 
 	y += 1.42 * PROP_HEIGHT * PROP_SMALL_SIZE_SCALE;
-	UI_DrawProportionalString( 320, y, "Game Designer", UI_CENTER|UI_SMALLFONT, color_white );
+	UI_DrawProportionalString(320, y, "Level Design", UI_CENTER | UI_SMALLFONT, colorLtGrey);
 	y += PROP_HEIGHT * PROP_SMALL_SIZE_SCALE;
-	UI_DrawProportionalString( 320, y, "Graeme Devine", UI_CENTER|UI_SMALLFONT, color_white );
+	UI_DrawProportionalString(320, y, "Michael 'mic' Denno,", UI_CENTER | UI_SMALLFONT, colorWhite);
+	y += PROP_HEIGHT * PROP_SMALL_SIZE_SCALE;
+	UI_DrawProportionalString(320, y, "'Dominic 'cha0s' Szablewski", UI_CENTER | UI_SMALLFONT, colorWhite);
 
 	y += 1.42 * PROP_HEIGHT * PROP_SMALL_SIZE_SCALE;
-	UI_DrawProportionalString( 320, y, "Level Design", UI_CENTER|UI_SMALLFONT, color_white );
+	UI_DrawProportionalString(320, y, "Project Leader", UI_CENTER | UI_SMALLFONT, colorLtGrey);
 	y += PROP_HEIGHT * PROP_SMALL_SIZE_SCALE;
-	UI_DrawProportionalString( 320, y, "Tim Willits, Christian Antkow, Paul Jaquays", UI_CENTER|UI_SMALLFONT, color_white );
+	UI_DrawProportionalString(320, y, "Robert 'Tr3B' Beckebans", UI_CENTER | UI_SMALLFONT, colorWhite);
+
+	/*
+	y += 1.42 * PROP_HEIGHT * PROP_SMALL_SIZE_SCALE;
+	UI_DrawProportionalString(320, y, "Director of Business Development", UI_CENTER | UI_SMALLFONT, colorWhite);
+	y += PROP_HEIGHT * PROP_SMALL_SIZE_SCALE;
+	UI_DrawProportionalString(320, y, "-", UI_CENTER | UI_SMALLFONT, colorWhite);
 
 	y += 1.42 * PROP_HEIGHT * PROP_SMALL_SIZE_SCALE;
-	UI_DrawProportionalString( 320, y, "CEO", UI_CENTER|UI_SMALLFONT, color_white );
+	UI_DrawProportionalString( 320, y, "Biz Assist and id Mom", UI_CENTER|UI_SMALLFONT, colorWhite );
 	y += PROP_HEIGHT * PROP_SMALL_SIZE_SCALE;
-	UI_DrawProportionalString( 320, y, "Todd Hollenshead", UI_CENTER|UI_SMALLFONT, color_white );
-
+	UI_DrawProportionalString( 320, y, "Donna Jackson", UI_CENTER|UI_SMALLFONT, colorWhite );
+	*/
+	
 	y += 1.42 * PROP_HEIGHT * PROP_SMALL_SIZE_SCALE;
-	UI_DrawProportionalString( 320, y, "Director of Business Development", UI_CENTER|UI_SMALLFONT, color_white );
+	UI_DrawProportionalString(320, y, "Development Assistance", UI_CENTER | UI_SMALLFONT, colorLtGrey);
 	y += PROP_HEIGHT * PROP_SMALL_SIZE_SCALE;
-	UI_DrawProportionalString( 320, y, "Marty Stratton", UI_CENTER|UI_SMALLFONT, color_white );
-
-	y += 1.42 * PROP_HEIGHT * PROP_SMALL_SIZE_SCALE;
-	UI_DrawProportionalString( 320, y, "Biz Assist and id Mom", UI_CENTER|UI_SMALLFONT, color_white );
+	UI_DrawProportionalString(320, y, "Mathias 'Skynet' Heyer", UI_CENTER | UI_SMALLFONT, colorWhite);
 	y += PROP_HEIGHT * PROP_SMALL_SIZE_SCALE;
-	UI_DrawProportionalString( 320, y, "Donna Jackson", UI_CENTER|UI_SMALLFONT, color_white );
-
-	y += 1.42 * PROP_HEIGHT * PROP_SMALL_SIZE_SCALE;
-	UI_DrawProportionalString( 320, y, "Development Assistance", UI_CENTER|UI_SMALLFONT, color_white );
+	UI_DrawProportionalString(320, y, "Josef 'cnuke' Soentgen", UI_CENTER | UI_SMALLFONT, colorWhite);
 	y += PROP_HEIGHT * PROP_SMALL_SIZE_SCALE;
-	UI_DrawProportionalString( 320, y, "Eric Webb", UI_CENTER|UI_SMALLFONT, color_white );
+	UI_DrawProportionalString(320, y, "Bjoern 'bj0ern' Paschen", UI_CENTER | UI_SMALLFONT, colorWhite);
 
 	y += 1.35 * PROP_HEIGHT * PROP_SMALL_SIZE_SCALE;
-	UI_DrawString( 320, y, "To order: 1-800-idgames     www.quake3arena.com     www.idsoftware.com", UI_CENTER|UI_SMALLFONT, color_red );
+//	UI_DrawString(320, y, "To order: 1-800-idgames     www.quake3arena.com     www.idsoftware.com", UI_CENTER | UI_SMALLFONT, colorRed);
+	UI_DrawString(320, y, "websites:   www.sourceforge.net/projects/xreal    xreal.sourceforge.net", UI_CENTER | UI_SMALLFONT, colorBlue);
+	y += 1.35 * PROP_HEIGHT * PROP_SMALL_SIZE_SCALE;
+	//y += SMALLCHAR_HEIGHT;
+	UI_DrawString(320, y, "Quake III Arena(c) 1999-2005, Id Software, Inc.", UI_CENTER | UI_SMALLFONT, colorRed);
 	y += SMALLCHAR_HEIGHT;
-	UI_DrawString( 320, y, "Quake III Arena(c) 1999-2000, Id Software, Inc.  All Rights Reserved", UI_CENTER|UI_SMALLFONT, color_red );
+	UI_DrawString(320, y, "XreaL(c) 2005-2006, XreaL team and contributors", UI_CENTER | UI_SMALLFONT, colorRed);
 }
 
 
@@ -119,11 +141,12 @@ static void UI_CreditMenu_Draw( void ) {
 UI_CreditMenu
 ===============
 */
-void UI_CreditMenu( void ) {
-	memset( &s_credits, 0 ,sizeof(s_credits) );
+void UI_CreditMenu(void)
+{
+	memset(&s_credits, 0, sizeof(s_credits));
 
 	s_credits.menu.draw = UI_CreditMenu_Draw;
 	s_credits.menu.key = UI_CreditMenu_Key;
 	s_credits.menu.fullscreen = qtrue;
-	UI_PushMenu ( &s_credits.menu );
+	UI_PushMenu(&s_credits.menu);
 }
