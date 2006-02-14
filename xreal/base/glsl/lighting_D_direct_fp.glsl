985351108bf8542cc46b63480900f70bda94ee88
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2006-02-14 11:53:17 +0000

    changed copyright notice

--- xreal/base/glsl/lighting_D_direct_fp.glsl
+++ xreal/base/glsl/lighting_D_direct_fp.glsl
@@ -1,24 +1,24 @@
-/// ============================================================================
 /*
-Copyright (C) 2005 Robert Beckebans <trebor_7@users.sourceforge.net>
-Please see the file "AUTHORS" for a list of contributors
+===========================================================================
+Copyright (C) 2006 Robert Beckebans <trebor_7@users.sourceforge.net>
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
+This file is part of XreaL source code.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+XreaL source code is free software; you can redistribute it
+and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of the License,
+or (at your option) any later version.
 
-See the GNU General Public License for more details.
+XreaL source code is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+along with XreaL source code; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+===========================================================================
 */
-/// ============================================================================
 
 uniform sampler2D	u_DiffuseMap;
 uniform vec3		u_AmbientColor;
