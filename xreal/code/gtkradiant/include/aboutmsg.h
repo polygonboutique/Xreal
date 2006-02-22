3e071c4ea8065a934878d0ffe6d50a8473848c2b
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2006-02-22 01:39:33 +0000

    added XreaL

--- xreal/code/gtkradiant/include/aboutmsg.h
+++ xreal/code/gtkradiant/include/aboutmsg.h
@@ -1,2 +1,2 @@
 // generated header, see makeversion.py
-#define RADIANT_ABOUTMSG "Custom build based on revision 22\ngcc version: 3.4.4"
+#define RADIANT_ABOUTMSG "XreaL build based on revision 23\ngcc version: 3.4.4"
