94f3eb9bc1be6825cf2a4f2568ba4efc29b53865
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2006-09-08 21:25:51 +0000

    cleaned screen_vp.glsl

--- xreal/base/glsl/screen_vp.glsl
+++ xreal/base/glsl/screen_vp.glsl
@@ -20,8 +20,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
 
-attribute vec4		attr_TexCoord0;
-
 void	main()
 {
 	// transform vertex position into homogenous clip-space
