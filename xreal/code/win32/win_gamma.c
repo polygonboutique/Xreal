5d992548a3214a7116d7aac0e0b500c493a448c2
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2007-05-14 22:16:48 +0000

    applied xreal-1328-vista_fixes.patch by raynorpat

--- xreal/code/win32/win_gamma.c
+++ xreal/code/win32/win_gamma.c
@@ -100,33 +100,6 @@ void WG_CheckHardwareGamma(void)
 }
 
 /*
-void mapGammaMax( void ) {
-	int		i, j;
-	unsigned short table[3][256];
-
-	// try to figure out what win2k will let us get away with setting
-	for ( i = 0 ; i < 256 ; i++ ) {
-		if ( i >= 128 ) {
-			table[0][i] = table[1][i] = table[2][i] = 0xffff;
-		} else {
-			table[0][i] = table[1][i] = table[2][i] = i<<9;
-		}
-	}
-
-	for ( i = 0 ; i < 128 ; i++ ) {
-		for ( j = i*2 ; j < 255 ; j++ ) {
-			table[0][i] = table[1][i] = table[2][i] = j<<8;
-			if ( !SetDeviceGammaRamp( glw_state.hDC, table ) ) {
-				break;
-			}
-		}
-		table[0][i] = table[1][i] = table[2][i] = i<<9;
-		Com_Printf( "index %i max: %i\n", i, j-1 );
-	}
-}
-*/
-
-/*
 ** GLimp_SetGamma
 **
 ** This routine should only be called if glConfig.deviceSupportsGamma is TRUE
@@ -143,8 +116,6 @@ void GLimp_SetGamma(unsigned char red[256], unsigned char green[256], unsigned c
 		return;
 	}
 
-//mapGammaMax();
-
 	for(i = 0; i < 256; i++)
 	{
 		table[0][i] = (((unsigned short)red[i]) << 8) | red[i];
@@ -152,30 +123,27 @@ void GLimp_SetGamma(unsigned char red[256], unsigned char green[256], unsigned c
 		table[2][i] = (((unsigned short)blue[i]) << 8) | blue[i];
 	}
 
-	// Win2K puts this odd restriction on gamma ramps...
+	// Win2K and newer put this odd restriction on gamma ramps...
 	vinfo.dwOSVersionInfoSize = sizeof(vinfo);
 	GetVersionEx(&vinfo);
-	if(vinfo.dwMajorVersion == 5 && vinfo.dwPlatformId == VER_PLATFORM_WIN32_NT)
+	if(vinfo.dwMajorVersion >= 5 && vinfo.dwPlatformId == VER_PLATFORM_WIN32_NT)
 	{
-		Com_DPrintf("performing W2K gamma clamp.\n");
+		Com_DPrintf("performing gamma clamp.\n");
 		for(j = 0; j < 3; j++)
 		{
 			for(i = 0; i < 128; i++)
 			{
-				if(table[j][i] > ((128 + i) << 8))
-				{
-					table[j][i] = (128 + i) << 8;
-				}
+				if(table[j][i] > ((128+i) << 8))
+					table[j][i] = (128+i) << 8;
 			}
-			if(table[j][127] > 254 << 8)
-			{
-				table[j][127] = 254 << 8;
-			}
+
+			if(table[j][127] > 254<<8)
+				table[j][127] = 254<<8;
 		}
 	}
 	else
 	{
-		Com_DPrintf("skipping W2K gamma clamp.\n");
+		Com_DPrintf("skipping gamma clamp.\n");
 	}
 
 	// enforce constantly increasing
