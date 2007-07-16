cd3fe30a0e8348f5fdbae75c45e44a1cec5f58f7
raynorpat <raynorpat@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2007-07-16 17:00:20 +0000

    re enable loadconfig and saveconfig

--- xreal/code/q3_ui/ui_atoms.c
+++ xreal/code/q3_ui/ui_atoms.c
@@ -1008,8 +1008,8 @@ void UI_Cache_f(void)
 	UI_AddBots_Cache();
 	UI_RemoveBots_Cache();
 	UI_SetupMenu_Cache();
-//  UI_LoadConfig_Cache();
-//  UI_SaveConfigMenu_Cache();
+	UI_LoadConfig_Cache();
+	UI_SaveConfigMenu_Cache();
 	UI_BotSelectMenu_Cache();
 	UI_ModsMenu_Cache();
 }
