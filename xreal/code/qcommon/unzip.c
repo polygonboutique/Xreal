6989a155c1edf03400c6baef836c27b791ec2152
raynorpat <raynorpat@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2007-12-15 00:49:31 +0000

    two ioquake3 fixes

--- xreal/code/qcommon/unzip.c
+++ xreal/code/qcommon/unzip.c
@@ -3973,7 +3973,7 @@ int inflateInit2_(z_streamp z, int w, const char *version, int stream_size)
 	z->state->wbits = (uInt) w;
 
 	/* create inflate_blocks state */
-	if((z->state->blocks = inflate_blocks_new(z, z->state->nowrap ? Z_NULL : adler32, (uInt) 1 << w)) == Z_NULL)
+	if((z->state->blocks = inflate_blocks_new(z, z->state->nowrap ? ((check_func)0) : adler32, (uInt)1 << w)) == Z_NULL)
 	{
 		inflateEnd(z);
 		return Z_MEM_ERROR;
