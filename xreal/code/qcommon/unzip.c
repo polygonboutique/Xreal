8a3571a3dbf3c79548941d6d74b79a030d613e0c
raynorpat <raynorpat@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2008-01-13 15:37:38 +0000

    silence warning in unzip.c

--- xreal/code/qcommon/unzip.c
+++ xreal/code/qcommon/unzip.c
@@ -1387,7 +1387,6 @@ extern int ZEXPORT unzGetGlobalComment(file, szComment, uSizeBuf)
 	 char           *szComment;
 	 uLong uSizeBuf;
 {
-	int             err = UNZ_OK;
 	unz_s          *s;
 	uLong           uReadThis;
 
