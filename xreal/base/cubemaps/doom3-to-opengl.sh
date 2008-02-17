bdc4526bbf17d0c4f7960a8a39ad35f2276e4260
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2008-02-17 15:35:25 +0000

    removed svn:eol-style native from doom3-to-opengl.sh

--- xreal/base/cubemaps/doom3-to-opengl.sh
+++ xreal/base/cubemaps/doom3-to-opengl.sh
@@ -1,10 +1,11 @@
+#!/bin/sh
 echo "converting $1 ..."
 
-convert $1_forward.tga -flip -rotate 90 $1_px.tga
-convert $1_back.tga -flip -rotate -90 $1_nx.tga
+convert $1_forward.tga -flip -rotate 90 $1_px.png
+convert $1_back.tga -flip -rotate -90 $1_nx.png
 
-convert $1_left.tga -flip $1_py.tga
-convert $1_right.tga -flop $1_ny.tga
+convert $1_left.tga -flip $1_py.png
+convert $1_right.tga -flop $1_ny.png
 
-convert $1_up.tga -flip -rotate 90 $1_pz.tga
-convert $1_down.tga -flop -rotate -90 $1_nz.tga
+convert $1_up.tga -flip -rotate 90 $1_pz.png
+convert $1_down.tga -flop -rotate -90 $1_nz.png
