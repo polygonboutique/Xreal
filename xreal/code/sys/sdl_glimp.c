3caf73db8ed4c95310db4e62532c5d805fb7bc22
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2008-08-29 20:02:48 +0000

    moved OpenGL 3.0 context initialization to GLimp_SetMode

--- xreal/code/sys/sdl_glimp.c
+++ xreal/code/sys/sdl_glimp.c
@@ -427,6 +427,32 @@ static int GLimp_SetMode(int mode, qboolean fullscreen)
 		break;
 	}
 
+#if defined(WIN32)
+	// try to initialize an OpenGL 3.0 context
+	qwglCreateContextAttribsARB = SDL_GL_GetProcAddress("wglCreateContextAttribsARB");
+	if(qwglCreateContextAttribsARB)
+	{
+		int             attribs[3];
+
+		ri.Printf(PRINT_ALL, "Initializing OpenGL 3.0 context...");
+
+		attribs[0] = WGL_CONTEXT_MAJOR_VERSION_ARB;
+		attribs[1] = 3;
+		attribs[2] = 0; //terminate first pair
+
+		opengl_context->hGLRC = qwglCreateContextAttribsARB(opengl_context->hDC, opengl_context->hGLRC, attribs);
+		if(wglMakeCurrent(opengl_context->hDC, opengl_context->hGLRC))
+		{
+			ri.Printf(PRINT_ALL, " done\n");
+			glConfig.driverType = GLDRV_OPENGL3;
+		}
+		else
+		{
+			ri.Printf(PRINT_ALL, " failed\n");
+		}
+	}
+#endif
+
 	QGL_Init();
 
 	GLimp_DetectAvailableModes();
@@ -1120,32 +1146,6 @@ void GLimp_Init(void)
 	glConfig.driverType = GLDRV_DEFAULT;
 	glConfig.hardwareType = GLHW_GENERIC;
 	glConfig.deviceSupportsGamma = !!(SDL_SetGamma(1.0f, 1.0f, 1.0f) >= 0);
-
-#if defined(WIN32)
-	// try to initialize an OpenGL 3.0 context
-	qwglCreateContextAttribsARB = SDL_GL_GetProcAddress("wglCreateContextAttribsARB");
-	if(qwglCreateContextAttribsARB)
-	{
-		int             attribs[3];
-
-		ri.Printf(PRINT_ALL, "Initializing OpenGL 3.0 context...");
-
-		attribs[0] = WGL_CONTEXT_MAJOR_VERSION_ARB;
-		attribs[1] = 3;
-		attribs[2] = 0; //terminate first pair
-
-		opengl_context->hGLRC = qwglCreateContextAttribsARB(opengl_context->hDC, opengl_context->hGLRC, attribs);
-		if(wglMakeCurrent(opengl_context->hDC, opengl_context->hGLRC))
-		{
-			ri.Printf(PRINT_ALL, " done\n");
-			glConfig.driverType = GLDRV_OPENGL3;
-		}
-		else
-		{
-			ri.Printf(PRINT_ALL, " failed\n");
-		}
-	}
-#endif
 
 	// get our config strings
 	Q_strncpyz(glConfig.vendor_string, (char *)qglGetString(GL_VENDOR), sizeof(glConfig.vendor_string));
