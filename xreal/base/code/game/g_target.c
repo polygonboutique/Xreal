2c46c49a3db19e70e08a1fa76d48774a205fefc8
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2008-12-16 22:31:13 +0000

    changed speaker entity default sound epair from s_shader to s_sound

--- xreal/base/code/game/g_target.c
+++ xreal/base/code/game/g_target.c
@@ -240,8 +240,18 @@ void SP_target_speaker(gentity_t * ent)
 	char            buffer[MAX_QPATH];
 	char           *s;
 
-	if(G_SpawnString("s_shader", "NOSOUND", &s))
+	if(G_SpawnString("s_sound", "NOSOUND", &s))
 	{
+		G_SpawnBoolean("s_looping", "0", &ent->soundLooping);
+		G_SpawnBoolean("s_waitfortrigger", "0", &ent->soundWaitForTrigger);
+		G_SpawnBoolean("s_global", "0", &ent->soundGlobal);
+		G_SpawnBoolean("s_activator", "0", &ent->soundActivator);
+		G_SpawnFloat("wait", "0", &ent->wait);
+		G_SpawnFloat("random", "0", &ent->random);
+	}
+	else if(G_SpawnString("s_shader", "NOSOUND", &s))
+	{
+		// Doom3 compatibility mode
 		G_SpawnBoolean("s_looping", "0", &ent->soundLooping);
 		G_SpawnBoolean("s_waitfortrigger", "0", &ent->soundWaitForTrigger);
 		G_SpawnBoolean("s_global", "0", &ent->soundGlobal);
