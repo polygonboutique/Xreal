a82beb0481b07137f27404e27a5f5a396b803f64
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2008-12-17 22:58:24 +0000

    changed XreaLRadiant's model selector to search for MD3 models as well

--- xreal/code/xrealradiant/radiant/ui/modelselector/ModelFileFunctor.h
+++ xreal/code/xrealradiant/radiant/ui/modelselector/ModelFileFunctor.h
@@ -15,13 +15,14 @@ namespace {
 	const char* ASE_EXTENSION = ".ase";
 	const char* LWO_EXTENSION = ".lwo";
 	const char* MD5MESH_EXTENSION = ".md5mesh";
+	const char* MD3_EXTENSION = ".md3";
 }
 
 /**
  * Functor object to visit the global VFS and add model paths to a VFS tree
  * populator object.
  */
-class ModelFileFunctor 
+class ModelFileFunctor
 {
 	// VFSTreePopulator to populate
 	gtkutil::VFSTreePopulator& _populator;
@@ -30,9 +31,9 @@ class ModelFileFunctor
 	gtkutil::ModalProgressDialog _progress;
 	int _count;
 	int _guiUpdateInterleave;
-	
+
 public:
-	
+
 	typedef const std::string& first_argument_type;
 
 	// Constructor sets the populator
@@ -51,12 +52,13 @@ public:
 		// Test the extension. If it is not matching any of the known extensions,
 		// not interested
 		if (boost::algorithm::iends_with(file, LWO_EXTENSION) ||
-			boost::algorithm::iends_with(file, ASE_EXTENSION) || 
-			boost::algorithm::iends_with(file, MD5MESH_EXTENSION)) 
+			boost::algorithm::iends_with(file, ASE_EXTENSION) ||
+			boost::algorithm::iends_with(file, MD5MESH_EXTENSION) ||
+			boost::algorithm::iends_with(file, MD3_EXTENSION))
 		{
 			_count++;
 			_populator.addPath(file);
-			
+
 			if (_count % _guiUpdateInterleave == 0) {
 				_progress.setText(boost::lexical_cast<std::string>(_count)
 								  + " models loaded");
