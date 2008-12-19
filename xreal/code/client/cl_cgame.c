56e31431f3688e7504f71057f0bfd6a1b89f5dc3
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2008-12-19 22:33:26 +0000

    killed obsolete CG_R_ADDADDITIVELIGHTTOSCENE

--- xreal/code/client/cl_cgame.c
+++ xreal/code/client/cl_cgame.c
@@ -651,9 +651,6 @@ intptr_t CL_CgameSystemCalls(intptr_t * args)
 		case CG_R_ADDLIGHTTOSCENE:
 			re.AddLightToScene(VMA(1), VMF(2), VMF(3), VMF(4), VMF(5));
 			return 0;
-		case CG_R_ADDADDITIVELIGHTTOSCENE:
-			re.AddAdditiveLightToScene(VMA(1), VMF(2), VMF(3), VMF(4), VMF(5));
-			return 0;
 		case CG_R_RENDERSCENE:
 			re.RenderScene(VMA(1));
 			return 0;
