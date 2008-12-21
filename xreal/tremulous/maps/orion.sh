8c079b58fa04e2aa8a53a0e567f4e21b6b2bf82d
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2008-12-21 21:51:08 +0000

    added orion.sh

---
+++ xreal/tremulous/maps/orion.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+COMPILER='../../xmap2.x86'
+MAP='orion.map'
+
+case $1 in
+	-bsp)
+		$COMPILER -fs_basepath ../.. -fs_game "tremulous" -game "xreal" -v -leaktest $MAP
+		;;
+
+	-fastvis)
+		$COMPILER -fs_basepath ../.. -fs_game "tremulous" -game "xreal" -vis -fast $MAP
+		;;
+		
+	-vis)
+		$COMPILER -fs_basepath ../.. -fs_game "tremulous" -game "xreal" -vis $MAP
+		;;
+
+	-light)
+		$COMPILER -fs_basepath ../.. -fs_game "tremulous" -game "xreal" -light -fast -filter -v -samplesize 4 -lightmapsize 2048 -super 2 -areascale 3.0 -pointscale 1.0 -skyscale 1.0 $MAP
+		;;
+
+	-all)
+		$COMPILER -fs_basepath ../.. -fs_game "tremulous" -game "xreal" -v -leaktest $MAP
+		# TODO
+		;;
+	*)
+		echo "specify command: -bsp, -fastvis, -vis, -light, or -all"
+		;;
+esac
+
