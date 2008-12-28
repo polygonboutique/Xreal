baa198c4a665aec0845399f480d7007d0574da41
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2008-12-28 23:28:26 +0000

    added some compile scripts for the test maps

---
+++ xreal/base/maps/test_portal_camera.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+COMPILER='../../xmap2.x86'
+MAP='test_portal_camera.map'
+
+case $1 in
+	-bsp)
+		$COMPILER -fs_basepath ../.. -fs_game "base" -game "xreal" -v -meta -leaktest $MAP
+		;;
+
+	-fastvis)
+		$COMPILER -fs_basepath ../.. -fs_game "base" -game "xreal" -vis -fast $MAP
+		;;
+		
+	-vis)
+		$COMPILER -fs_basepath ../.. -fs_game "base" -game "xreal" -vis $MAP
+		;;
+
+	-light)
+		$COMPILER -fs_basepath ../.. -fs_game "base" -game "xreal" -light -fast -filter -v -samplesize 1 -lightmapsize 2048 -super 2 -areascale 1.0 -pointscale 1.0 -skyscale 1.0 $MAP
+		;;
+
+	-all)
+		$COMPILER -fs_basepath ../.. -fs_game "base" -game "xreal" -v -leaktest $MAP
+		# TODO
+		;;
+		
+	-debugportals)
+		$COMPILER -fs_basepath ../.. -fs_game "base" -game "xreal" -v -meta -debugportals $MAP
+		;;
+	*)
+		echo "specify command: -bsp, -fastvis, -vis, -light, or -all"
+		;;
+esac
+
