0bcb87621f5af606642d48c3e73f5ebec675fe94
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2008-12-29 22:31:04 +0000

    updated tremulous map compile scripts

--- xreal/tremulous/maps/orion.sh
+++ xreal/tremulous/maps/orion.sh
@@ -1,31 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-COMPILER='../../xmap2.x86'
 MAP='orion.map'
+LIGHTOPTS='-samplesize 4 -lightmapsize 1024 -super 2 -areascale 3.0 -pointscale 1.0 -skyscale 1.0'
 
-case $1 in
-	-bsp)
-		$COMPILER -fs_basepath ../.. -fs_game "tremulous" -game "xreal" -v -meta -leaktest $MAP
-		;;
-
-	-fastvis)
-		$COMPILER -fs_basepath ../.. -fs_game "tremulous" -game "xreal" -vis -fast $MAP
-		;;
-		
-	-vis)
-		$COMPILER -fs_basepath ../.. -fs_game "tremulous" -game "xreal" -vis $MAP
-		;;
-
-	-light)
-		$COMPILER -fs_basepath ../.. -fs_game "tremulous" -game "xreal" -light -fast -filter -v -samplesize 4 -lightmapsize 2048 -super 2 -areascale 3.0 -pointscale 1.0 -skyscale 1.0 $MAP
-		;;
-
-	-all)
-		$COMPILER -fs_basepath ../.. -fs_game "tremulous" -game "xreal" -v -leaktest $MAP
-		# TODO
-		;;
-	*)
-		echo "specify command: -bsp, -fastvis, -vis, -light, or -all"
-		;;
-esac
-
+source compilerinfo
--- xreal/tremulous/maps/tremor.sh
+++ xreal/tremulous/maps/tremor.sh
@@ -1,31 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
-COMPILER='../../xmap2.x86'
 MAP='tremor.map'
+LIGHTOPTS='-samplesize 1 -lightmapsize 2048 -super 2 -pointscale 3.0 -skyscale 1.0'
 
-case $1 in
-	-bsp)
-		$COMPILER -fs_basepath ../.. -fs_game "tremulous" -game "xreal" -v -leaktest $MAP
-		;;
-
-	-fastvis)
-		$COMPILER -fs_basepath ../.. -fs_game "tremulous" -game "xreal" -vis -fast $MAP
-		;;
-		
-	-vis)
-		$COMPILER -fs_basepath ../.. -fs_game "tremulous" -game "xreal" -vis $MAP
-		;;
-
-	-light)
-		$COMPILER -fs_basepath ../.. -fs_game "tremulous" -game "xreal" -light -fast -filter -v -samplesize 1 -lightmapsize 2048 -super 2 -pointscale 3.0 -skyscale 1.0 $MAP
-		;;
-
-	-all)
-		$COMPILER -fs_basepath ../.. -fs_game "tremulous" -game "xreal" -v -leaktest $MAP
-		# TODO
-		;;
-	*)
-		echo "specify command: -bsp, -fastvis, -vis, -light, or -all"
-		;;
-esac
+source compilerinfo
 
