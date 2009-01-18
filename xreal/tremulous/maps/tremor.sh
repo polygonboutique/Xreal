6f9c2221782e4baaac56d4f9203463bb693899eb
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2009-01-18 21:12:25 +0000

    relaxed tremors compile options

--- xreal/tremulous/maps/tremor.sh
+++ xreal/tremulous/maps/tremor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 MAP='tremor.map'
-LIGHTOPTS='-fast -filter -super 2 -samplesize 1 -lightmapsize 2048 -pointscale 3.0 -skyscale 7.0 -areascale 10.0'
+LIGHTOPTS='-fast -filter -super 2 -samplesize 4 -lightmapsize 1024 -pointscale 2.0 -skyscale 3.0 -areascale 7.0 -bounce 0'
 
 source compilerinfo
 
