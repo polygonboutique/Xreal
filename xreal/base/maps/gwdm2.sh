1d5e02188a9178611ddf1477c2f7e932a97d5b08
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2009-01-20 21:37:39 +0000

    tweaked gwdm2 light compile options

--- xreal/base/maps/gwdm2.sh
+++ xreal/base/maps/gwdm2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 MAP='gwdm2.map'
-LIGHTOPTS='-fast -filter -samplesize 1 -lightmapsize 2048 -super 2 -pointscale 1.0 -skyscale 10.0 -areascale 1.0'
+LIGHTOPTS='-fast -filter -samplesize 1 -lightmapsize 2048 -super 2 -pointscale 1.0 -skyscale 2.0 -areascale 1.0 -bounce 0'
 
 source compilerinfo
 
