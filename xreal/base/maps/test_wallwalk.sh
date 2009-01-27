90d60d79194ced42b7c4040cf695b5d63fa38fc2
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2009-01-27 20:41:51 +0000

    added wallwalk test map including strogg style wallwalk materials

---
+++ xreal/base/maps/test_wallwalk.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+MAP='test_wallwalk.map'
+LIGHTOPTS='-fast -filter -super 2 -samplesize 1 -lightmapsize 1024 -pointscale 1.0 -skyscale 1.0 -areascale 1.0 -bounce 1'
+
+source compilerinfo
+
