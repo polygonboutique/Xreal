d6e47e70f256bad7555c74d48d4e01ba0ba7ba90
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2009-02-21 23:40:27 +0000

    added kat_q4dm3.sh

---
+++ xreal/base/maps/kat_q4dm3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+MAP='kat_q4dm3.map'
+LIGHTOPTS='-fast -filter -super 2 -samplesize 1 -lightmapsize 2048 -pointscale 1.0 -skyscale 1.0 -areascale 1.0 -bounce 0'
+
+source compilerinfo
+
