3e1a1c2af63ec8dd43ba41bb6ec7dea52043d578
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2009-03-02 17:12:47 +0000

    tweaked gwdm2 for HDR lighting

--- xreal/base/maps/gwdm2.sh
+++ xreal/base/maps/gwdm2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 MAP='gwdm2.map'
-LIGHTOPTS='-fast -filter -samplesize 1 -lightmapsize 2048 -super 2 -pointscale 1.0 -skyscale 2.0 -areascale 1.0 -bounce 0'
+LIGHTOPTS='-fast -filter -samplesize 1 -lightmapsize 1024 -super 2 -pointscale 2.0 -skyscale 5.0 -areascale 2.0 -bounce 1'
 
 source compilerinfo
 
