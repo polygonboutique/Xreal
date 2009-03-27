bc366e329f506ff9427223952070d98b0ff4e935
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2009-03-27 21:17:24 +0000

    applied partial macosx port patch by cnuke

--- xreal/code/client/qal.h
+++ xreal/code/client/qal.h
@@ -41,6 +41,9 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
   // MSVC users must install the OpenAL SDK which doesn't use the AL/*.h scheme.
 #include <al.h>
 #include <alc.h>
+#elif defined (MACOS_X)
+#include <OpenAL/al.h>
+#include <OpenAL/alc.h>
 #else
 #include <AL/al.h>
 #include <AL/alc.h>
