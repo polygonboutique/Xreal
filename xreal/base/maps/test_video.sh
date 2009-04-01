0f693a6a2dbea722ea591d3d6c5566a2b5ba7f3d
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2009-04-01 16:27:03 +0000

    updated .RoQ video spot light test map

---
+++ xreal/base/maps/test_video.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+MAP='test_video.map'
+LIGHTOPTS='-fast -filter -super 2 -samplesize 1 -lightmapsize 512 -pointscale 1.0 -skyscale 1.0 -areascale 1.0 -bounce 0'
+
+source compilerinfo
+
