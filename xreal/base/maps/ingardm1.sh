3e0399c624fb6c9affaa226c41e3318d172e2e63
megatog615 <megatog615@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2009-04-20 20:50:02 +0000

    Renamed 'gaelish' to 'ingardm1'. Added stormydays skybox by hipshot.

--- xreal/base/maps/ingardm1.sh
+++ xreal/base/maps/ingardm1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MAP='gaelish.map'
+MAP='ingardm1.map'
 LIGHTOPTS='-fast -filter -samplesize 1 -lightmapsize 1024 -super 2 -pointscale 3.0 -skyscale 2.0 -areascale 2.0 -bounce 1'
 
 source compilerinfo
