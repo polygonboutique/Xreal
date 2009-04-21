8fbeb88e66a8af4279b802d20e702adb9008870d
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2009-04-21 22:23:56 +0000

    tweaked redm08 light options

--- xreal/base/maps/redm08.sh
+++ xreal/base/maps/redm08.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 MAP='redm08.map'
-LIGHTOPTS='-fast -filter -super 2 -samplesize 1 -lightmapsize 1024 -pointscale 1.0 -skyscale 1.0 -areascale 1.0 -bounce 0'
+LIGHTOPTS='-fast -filter -super 2 -samplesize 1 -lightmapsize 1024 -floodlight -pointscale 1.0 -skyscale 1.0 -areascale 1.0 -bounce 0'
 
 source compilerinfo
 
