16158c36a5fb06c37515fc6b507daf7061f963d8
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2009-04-22 10:52:13 +0000

    added VISOPTS='-fast' to q4bolledm2.sh and yan_test.sh

--- xreal/base/maps/yan_test.sh
+++ xreal/base/maps/yan_test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 MAP='yan_test.map'
+VISOPTS='-fast'
 LIGHTOPTS='-fast -filter -super 2 -samplesize 1 -lightmapsize 2048 -pointscale 2.0 -skyscale 7.0 -areascale 1.0'
 
 source compilerinfo
