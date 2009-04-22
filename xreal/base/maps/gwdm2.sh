4c756565be44459668b3aaa93e817ba81dbb30e5
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2009-04-22 13:48:56 +0000

    added -floodlight option to gwdm2

--- xreal/base/maps/gwdm2.sh
+++ xreal/base/maps/gwdm2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 MAP='gwdm2.map'
-LIGHTOPTS='-fast -filter -samplesize 1 -lightmapsize 1024 -super 2 -pointscale 2.0 -skyscale 5.0 -areascale 2.0 -bounce 1'
+#VISOPTS='-fast'
+LIGHTOPTS='-fast -filter -samplesize 1 -lightmapsize 1024 -super 2 -floodlight -pointscale 2.0 -skyscale 5.0 -areascale 2.0 -bounce 0'
 
 source compilerinfo
 
