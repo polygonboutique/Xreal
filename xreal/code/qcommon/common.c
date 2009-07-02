9034a1901a221033b5a4198a0e0efdd9b640f2d2
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2009-07-02 16:59:54 +0000

    increased DEF_COMZONEMEGS to 96

--- xreal/code/qcommon/common.c
+++ xreal/code/qcommon/common.c
@@ -44,7 +44,7 @@ int             demo_protocols[] = { PROTOCOL_VERSION, 0 };
 #define MIN_DEDICATED_COMHUNKMEGS 1
 #define MIN_COMHUNKMEGS		96
 #define DEF_COMHUNKMEGS		512
-#define DEF_COMZONEMEGS		64
+#define DEF_COMZONEMEGS		96
 #define XSTRING(x)				STRING(x)
 #define STRING(x)					#x
 #define DEF_COMHUNKMEGS_S	XSTRING(DEF_COMHUNKMEGS)
