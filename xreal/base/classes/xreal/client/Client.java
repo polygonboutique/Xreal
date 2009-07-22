dda04879679377a8e67cfbb39de270d2a41c6e8b
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2009-07-22 11:28:55 +0000

    added client sound engine <-> Java bridge

--- xreal/base/classes/xreal/client/Client.java
+++ xreal/base/classes/xreal/client/Client.java
@@ -1,5 +1,8 @@
 package xreal.client;
 
+import javax.vecmath.Quat4f;
+import javax.vecmath.Vector3f;
+
 /**
  * Represents Client functionality. Never use this code for the server game.
  * 
@@ -13,6 +16,9 @@ public class Client {
 	 * @return If the index is < 0 or > MAX_CONFIGSTRINGS it will return null. 
 	 */
 	public synchronized native static String getConfigString(int index);
+	
+	
+	// keyboard and mouse event handling ----------------------------------------------------------
 	
 	/**
 	 * Get all current key catchers defined in KeyCatchers
@@ -49,4 +55,93 @@ public class Client {
 	 * Issue all remaining key events and reset each key state.
 	 */
 	public synchronized native static void clearKeyStates();
+	
+	
+	
+	// sound handling -----------------------------------------------------------------------------
+	
+	/**
+	 * Load a sound into the sound engine, if it's not already loaded.
+	 * 
+	 * @return A handle of type sfxHandle_t (int). Returns buzz if not found.
+	 */
+	public synchronized native static int registerSound(String fileName);
+	
+	
+	/**
+	 * Play a sound at the given location.
+	 * 
+	 * Normal sounds will have their volume dynamically changed as their entity moves and the listener moves.
+	 * 
+	 * @param posX
+	 * @param posY
+	 * @param posZ
+	 * @param entityNum
+	 * @param entityChannel
+	 * @param sfxHandle
+	 */
+	public synchronized native static void startSound(float posX, float posY, float posZ, int entityNum, int entityChannel, int sfxHandle);
+	
+	public static void startSound(Vector3f pos, int entityNum, int entityChannel, int sfxHandle) {
+		startSound(pos.x, pos.y, pos.z, entityNum, entityChannel, sfxHandle);
+	}
+	
+	/**
+	 * Play a sound without distance attenuation. A local sound is always played full volume.
+	 * 
+	 * @param sfx
+	 * @param channelNum
+	 */
+	public synchronized native static void startLocalSound(int sfxHandle, int channelNum);
+	
+	public synchronized native static void addLoopingSound(int entityNum, float posX, float posY, float posZ, float velX, float velY, float velZ, int sfxHandle);
+	
+	public static void addLoopingSound(int entityNum, Vector3f position, Vector3f velocity, int sfxHandle) {
+		addLoopingSound(entityNum, position.x, position.y, position.z, velocity.x, velocity.y, velocity.z, sfxHandle);
+	}
+	
+	public synchronized native static void addRealLoopingSound(int entityNum, float posX, float posY, float posZ, float velX, float velY, float velZ, int sfxHandle);
+	
+	public static void addRealLoopingSound(int entityNum, Vector3f position, Vector3f velocity, int sfxHandle) {
+		addRealLoopingSound(entityNum, position.x, position.y, position.z, velocity.x, velocity.y, velocity.z, sfxHandle);
+	}
+	
+	/**
+	 * Let the sound system know where an entity currently is.
+	 * 
+	 * @param entityNum
+	 */
+	public synchronized native static void updateEntitySoundPosition(int entityNum, float posX, float posY, float posZ);
+	
+	public static void updateEntitySoundPosition(int entityNum, Vector3f position) {
+		updateEntitySoundPosition(entityNum, position.x, position.y, position.z);
+	}
+	
+	public synchronized native static void stopLoopingSound(int entityNum);
+	
+	public synchronized native static void clearLoopingSounds(boolean killall);
+
+	/**
+	 * Recalculate the volumes of sound as they should be heard by the given entityNum and position.
+	 * 
+	 * @param entityNum
+	 */
+	public synchronized native static void respatialize(int entityNum, float posX, float posY, float posZ, float quatX, float quatY, float quatZ, float quatW, boolean inWater);
+	
+	public static void respatialize(int entityNum, Vector3f position, Quat4f orientation, boolean inWater) {
+		respatialize(entityNum, position.x, position.y, position.z, orientation.x, orientation.y, orientation.z, orientation.w, inWater);
+	}
+	
+	
+	/**
+	 * Start a background track.
+	 * Empty name stops music.
+	 * 
+	 * @param intro
+	 * @param loop
+	 */
+	public synchronized native static void startBackgroundTrack(String intro, String loop);
+	public synchronized native static void stopBackgroundTrack();
+	
+	
 }
