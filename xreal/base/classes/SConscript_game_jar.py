b628db036f48031ed888ec963c8c2f1b15116e23
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2009-07-23 10:48:59 +0000

    removed more junk

--- xreal/base/classes/SConscript_game_jar.py
+++
@@ -1,9 +0,0 @@
-import os
-
-Import('env')
-
-#env.Jar(target = 'game.jar', source = ['com', 'javax', 'xreal'])
-#os.system('rm ../game.jar')
-#os.remove('../game.jar')
-os.system('zip ../game.jar -r META-INF com javax xreal -x "*.svn*" -x "*.java" ')
-
