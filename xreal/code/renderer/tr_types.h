b6b4334de9cf67ac12d27e7e4245d457a2c260a0
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2009-09-14 23:30:44 +0000

    reverted MAX_REF_ENTITIES

--- xreal/code/renderer/tr_types.h
+++ xreal/code/renderer/tr_types.h
@@ -26,7 +26,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 
 #define	MAX_REF_LIGHTS		1024
-#define	MAX_REF_ENTITIES	512	// can't be increased without changing drawsurf bit packing
+#define	MAX_REF_ENTITIES	1023	// can't be increased without changing drawsurf bit packing
 #define MAX_BONES      	 	120
 #define MAX_WEIGHTS			4	// GPU vertex skinning limit, never change this without rewriting many GLSL shaders
 
