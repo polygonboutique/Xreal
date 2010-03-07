e3d2ad846410d6e898c841922f9f62a96d15c44f
dersaidin <dersaidin@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2010-03-07 12:50:48 +0000

    Fixes for lua library, and added more debug output in lua_game.

--- xreal/base/code/game/lua_game.c
+++ xreal/base/code/game/lua_game.c
@@ -34,6 +34,9 @@ static int game_Print(lua_State * L)
 	memset(buf, 0, sizeof(buf));
 
 	lua_getglobal(L, "tostring");
+
+	DEBUG_LUA("game_Print: start: ");
+
 	for(i = 1; i <= n; i++)
 	{
 		const char     *s;
@@ -44,7 +47,10 @@ static int game_Print(lua_State * L)
 		s = lua_tostring(L, -1);	// get result
 
 		if(s == NULL)
+		{
+			DEBUG_LUA("game_Print: return: no string");
 			return luaL_error(L, "`tostring' must return a string to `print'");
+		}
 
 		Q_strcat(buf, sizeof(buf), s);
 
@@ -52,6 +58,8 @@ static int game_Print(lua_State * L)
 	}
 
 	G_Printf("%s\n", buf);
+
+	DEBUG_LUA("game_Print: return: printed string");
 	return 0;
 }
 
@@ -64,6 +72,9 @@ static int game_Broadcast(lua_State * L)
 	memset(buf, 0, sizeof(buf));
 
 	lua_getglobal(L, "tostring");
+
+	DEBUG_LUA("game_Broadcast: start: ");
+
 	for(i = 1; i <= n; i++)
 	{
 		const char     *s;
@@ -74,7 +85,10 @@ static int game_Broadcast(lua_State * L)
 		s = lua_tostring(L, -1);	// get result
 
 		if(s == NULL)
+		{
+			DEBUG_LUA("game_Broadcast: return: no string");
 			return luaL_error(L, "`tostring' must return a string to `print'");
+		}
 
 		Q_strcat(buf, sizeof(buf), s);
 
@@ -82,6 +96,8 @@ static int game_Broadcast(lua_State * L)
 	}
 
 	trap_SendServerCommand(-1, va("cp \"" S_COLOR_WHITE "%s\n\"", buf));
+
+	DEBUG_LUA("game_Broadcast: return: broadcasted string");
 	return 0;
 }
 
@@ -92,12 +108,15 @@ static int game_SpawnGroupRedDisable(lua_State * L)
 
 	spawnGroup = luaL_checkint(L, 1);
 
+	DEBUG_LUA("game_SpawnGroupRedDisable: start: group=%d", spawnGroup);
+
 	next = NULL;
 	do
 	{
 		next = G_Find(next, FOFS(classname), "team_ctf_redspawn");
 		if(!next)
 		{
+			DEBUG_LUA("game_SpawnGroupRedDisable: return: next spawn not found");
 			return 0;
 		}
 		if(next->group == spawnGroup)
@@ -106,6 +125,7 @@ static int game_SpawnGroupRedDisable(lua_State * L)
 		}
 	} while(!Q_stricmp(next->classname, "team_ctf_redspawn"));
 
+	DEBUG_LUA("game_SpawnGroupRedDisable: return: no more spawns");
 	return 0;
 }
 
@@ -116,12 +136,15 @@ static int game_SpawnGroupBlueDisable(lua_State * L)
 
 	spawnGroup = luaL_checkint(L, 1);
 
+	DEBUG_LUA("game_SpawnGroupBlueDisable: start: group=%d", spawnGroup);
+
 	next = NULL;
 	do
 	{
 		next = G_Find(next, FOFS(classname), "team_ctf_bluespawn");
 		if(!next)
 		{
+			DEBUG_LUA("game_SpawnGroupBlueDisable: return: next spawn not found");
 			return 0;
 		}
 		if(next->group == spawnGroup)
@@ -130,6 +153,7 @@ static int game_SpawnGroupBlueDisable(lua_State * L)
 		}
 	} while(!Q_stricmp(next->classname, "team_ctf_bluespawn"));
 
+	DEBUG_LUA("game_SpawnGroupBlueDisable: return: no more spawns");
 	return 0;
 }
 
@@ -140,12 +164,15 @@ static int game_SpawnGroupRedEnable(lua_State * L)
 
 	spawnGroup = luaL_checkint(L, 1);
 
+	DEBUG_LUA("game_SpawnGroupRedEnable: start: group=%d", spawnGroup);
+
 	next = NULL;
 	do
 	{
 		next = G_Find(next, FOFS(classname), "team_ctf_redspawn");
 		if(!next)
 		{
+			DEBUG_LUA("game_SpawnGroupRedEnable: return: next spawn not found");
 			return 0;
 		}
 		if(next->group == spawnGroup)
@@ -154,6 +181,7 @@ static int game_SpawnGroupRedEnable(lua_State * L)
 		}
 	} while(!Q_stricmp(next->classname, "team_ctf_redspawn"));
 
+	DEBUG_LUA("game_SpawnGroupRedEnable: return: no more spawns");
 	return 0;
 }
 
@@ -164,12 +192,15 @@ static int game_SpawnGroupBlueEnable(lua_State * L)
 
 	spawnGroup = luaL_checkint(L, 1);
 
+	DEBUG_LUA("game_SpawnGroupBlueEnable: start: group=%d", spawnGroup);
+
 	next = NULL;
 	do
 	{
 		next = G_Find(next, FOFS(classname), "team_ctf_bluespawn");
 		if(!next)
 		{
+			DEBUG_LUA("game_SpawnGroupBlueEnable: return: next spawn not found");
 			return 0;
 		}
 		if(next->group == spawnGroup)
@@ -178,15 +209,19 @@ static int game_SpawnGroupBlueEnable(lua_State * L)
 		}
 	} while(!Q_stricmp(next->classname, "team_ctf_bluespawn"));
 
+	DEBUG_LUA("game_SpawnGroupBlueEnable: return: no more spawns");
 	return 0;
 }
 
 // game.EndRound()
 static int game_EndRound(lua_State * L)
 {
-	//DEBUG_LUA("et_GameEnd: start: round ending");
+	DEBUG_LUA("game_EndRound: start: round ending");
+
 	trap_SendServerCommand(-1, "print \"Round Ended.\n\"");
 	LogExit("Round Ended.");
+
+	DEBUG_LUA("game_EndRound: return: exited");
 	return 0;
 }
 
@@ -194,6 +229,8 @@ static int game_EndRound(lua_State * L)
 static int game_Leveltime(lua_State * L)
 {
 	lua_pushinteger(L, level.time);
+
+	DEBUG_LUA("game_Leveltime: start/return: leveltime=%d", level.time);
 	return 1;
 }
 
