e5505c36a40a5352edbbc7c27384da56e8b77955
trebor_7 <trebor_7@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2011-08-19 10:12:27 +0000

    Added premake4_experimental-with-java_vs2010.bat

---
+++ xreal/javah_export-engine-interfaces.bat
@@ -0,0 +1,10 @@
+javah -classpath quaplexity/classes -d code/engine/qcommon/java xreal.Engine
+javah -classpath quaplexity/classes -d code/engine/qcommon/java xreal.CVar
+
+javah -classpath quaplexity/classes -d code/engine/server/java xreal.server.Server
+javah -classpath quaplexity/classes -d code/engine/server/java xreal.server.game.GameEntity
+javah -classpath quaplexity/classes -d code/engine/server/java xreal.server.game.Player
+
+javah -classpath quaplexity/classes -d code/engine/client/java xreal.client.Client
+javah -classpath quaplexity/classes -d code/engine/client/java xreal.client.renderer.Renderer
+pause
