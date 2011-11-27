f9a4dc731be77430d90b79c3b509de829594c309
dersaidin <dersaidin@8d6dca30-e10d-0410-ba00-f2efb4c278cc>
2011-11-27 05:16:27 +0000

    Fix declaration of for new versions of FS functions.

--- xreal/code/engine/qcommon/qcommon.h
+++ xreal/code/engine/qcommon/qcommon.h
@@ -675,11 +675,11 @@ fileHandle_t    FS_FOpenFileAppend(const char *filename);
 
 // will properly create any needed paths and deal with seperater character issues
 
-int             FS_filelength(fileHandle_t f);
+long            FS_filelength(fileHandle_t f);
 fileHandle_t    FS_SV_FOpenFileWrite(const char *filename);
 int             FS_SV_FOpenFileRead(const char *filename, fileHandle_t * fp);
 void            FS_SV_Rename(const char *from, const char *to);
-int             FS_FOpenFileRead(const char *qpath, fileHandle_t * file, qboolean uniqueFILE);
+long            FS_FOpenFileRead(const char *filename, fileHandle_t *file, qboolean uniqueFILE);
 
 // if uniqueFILE is true, then a new FILE will be fopened even if the file
 // is found in an already open pak file.  If uniqueFILE is false, you must call
